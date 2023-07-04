9e6b4a500c7339b4c19d7775439dc091938a92ba
koko462 <115919610+koko462@users.noreply.github.com>
2023-07-04 09:51:31 +0300

    Add files via upload

---
+++ FinalProjectVersion2.0/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[][] enterInputMatrix(int rows, int columns){
+        Scanner console = new Scanner(System.in);
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter elements of matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Element [" + i + "][" + j + "]: ");
+                matrix[i][j] = console.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        //Entering rows and columns of matrix
+        System.out.print("Enter rows of matrix: ");
+        int rows = console.nextInt();
+        System.out.print("Enter columns of matrix: ");
+        int columns = console.nextInt();
+
+        int[][] matrix = enterInputMatrix(rows, columns);
+        printMatrix(matrix);
+
+    }
+}
